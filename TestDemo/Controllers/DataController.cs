1963e9f9f0e0fddb85ffc092789fe5db47d21c2e
Sagar Maharana <maharanasagar379@gmail.com>
2021-04-25 23:09:22 +0530

    Add project files.

---
+++ TestDemo/Controllers/DataController.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Web.Http;
+using static TestDemo.Models.BusinessLogic;
+
+namespace TestDemo.Controllers
+{
+    [RoutePrefix("api")]
+    public class DataController : ApiController
+    {
+        [Route("signin")]
+        [HttpPost]
+        public async Task<IHttpActionResult> GetMethod(HttpRequestMessage message)
+        {
+            try
+            {
+                string token = string.Empty;
+                var req = await message.Content.ReadAsStringAsync();
+                var headers = message.Headers;
+                if (headers.Contains("Token"))
+                {
+                    token = headers.GetValues("Token").First();
+                }
+                string temptoken = "jhgjbjh";
+                if (token == temptoken)
+                {
+                    return Json(new
+                    {
+                        Message = "Invalid Token"
+                    });
+                }
+                var user = ConfigurationManager.AppSettings["Username"];
+                var pass = ConfigurationManager.AppSettings["Password"];
+                signin login = JsonConvert.DeserializeObject<signin>(req);
+                if (login.Username == user || login.Password == pass)
+                {
+                    return Json(new
+                    {
+                        Message = "Credentials is successfully authenticated"
+                    });
+                }
+                else
+                {
+                    return Json(new
+                    {
+                        Message = "Please check you login Credentials"
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                    Message = ex
+                });
+            }
+            
+        }
+        
+    }
+}
---
+++ TestDemo/Global.asax.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using TestDemo.Controllers;
+
+namespace TestDemo
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            CreateTimer();           
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+        public string Token()
+        {
+            string tokenvalue = RandomString(10);
+            return tokenvalue;
+        }
+
+        public void CreateTimer()
+        {
+            var timer = new System.Timers.Timer(10000); // fire every 1 second
+            timer.Elapsed += HandleTimerElapsed;
+            timer.Enabled = true;
+        }
+
+        public void HandleTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            Token();
+        }
+
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
